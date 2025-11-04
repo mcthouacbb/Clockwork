bb2b1ef6ec8ac643c003c6cad992c2be2ccedd01
mcthouacbb <mcthouacbb@gmail.com>
2025-11-03 19:07:31 -0500

    A

--- src/evaltune_main.cpp
+++ src/evaltune_main.cpp
@@ -30,9 +30,7 @@ int main() {
     std::vector<f64>      results;
 
     // List of files to load
-    const std::vector<std::string> fenFiles = {
-      "data/dfrc-1m.txt", "data/dfrcv0.txt", "data/v2.2.txt", "data/v2.1.txt", "data/v3/v3.txt",
-    };
+    const std::vector<std::string> fenFiles = {"C:/Users/Mcthouacbb/Documents/dist/1.book"};
 
     // Number of threads to use, default to half available
     const u32 thread_count = std::max<u32>(1, std::thread::hardware_concurrency() / 2);
